5bb872722e4be801fd12c0ad56ab4436d1e5584e
Angel <angelmiladinovv@gmail.com>
2018-07-31 09:28:09 +0200

    changed player sprite to empty, removed rotation

--- Assets/Scripts/ScreenTouch/ScreenTouch.cs
+++ Assets/Scripts/ScreenTouch/ScreenTouch.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DeltaDNA;
+using UnityEngine.SceneManagement;
 
 public class ScreenTouch : MonoBehaviour {
 
@@ -24,17 +25,46 @@ public class ScreenTouch : MonoBehaviour {
 
     private void Awake()
     {
+        print("Awake");
         playerRB = GetComponent<Rigidbody2D>();
+        //playerRB.velocity = new Vector2(0f, 1f);
+        //Invoke("AutoJump", 3f);
     }
 
     private void OnEnable()
     {
+        print("OnEnable");
         Vector2 force = new Vector2(0f, 10f);
-        playerRB.AddForce(force, ForceMode2D.Impulse);
+        //playerRB.AddForce(force, ForceMode2D.Impulse);
     }
 
-    private void Update()
+    private void Start()
     {
+        print("OnStart");
+    }
+
+    private void OnDisable()
+    {
+        print("OnDisable");
+    }
+
+    private void OnDestroy()
+    {
+        print("OnDestroy");
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        print("OnTriggerEnter2D");
+        //Vector2 force = new Vector2(0f, 15f);
+        //playerRB.AddForce(force, ForceMode2D.Impulse);
+
+        //playerRB.velocity = new Vector2(0f, 1);
+    }
+
+    private void FixedUpdate()
+    {
+        //print("Update");
         if(Input.touches.Length > 0)
         {
             Touch touch = Input.touches[0];
@@ -60,7 +90,7 @@ public class ScreenTouch : MonoBehaviour {
                 shouldMove = true;
                 prevDirection = direction;
                 direction = prevTouch.position.x < touch.position.x ? Direction.Righ : Direction.Left;
-                Rotate(prevDirection != direction);
+                //Rotate(prevDirection != direction);
                 prevTouch = touch;
                 // when you move the touch for second time
             }
@@ -83,7 +113,8 @@ public class ScreenTouch : MonoBehaviour {
         newX += direction == Direction.Left ? -1f * Time.deltaTime : 1f * Time.deltaTime; // depending on the direction increase / decrease the value of x by speed * Time*deltaTime. This way the object will move speed units per second
         Vector2 newPosition = new Vector2(newX, transform.position.y); // create vector with the old y and z values and the new x
 
-        transform.position = newPosition; // set the new position to the transform component i.e. move the object
+        //transform.position = newPosition; // set the new position to the transform component i.e. move the object
+        playerRB.MovePosition(newPosition);
         /*
         if(direction == Direction.Left)
         {
@@ -100,15 +131,24 @@ public class ScreenTouch : MonoBehaviour {
             print(moveBy);
         }*/
     }
-
+    /*
     private void Rotate(bool changeDirection) // whenever we turn the direction the object should be inverted
     {
         if (changeDirection) // The boolean variable changeDirection tells us should we change the direction
         {
             Vector3 currentRotation = transform.rotation.eulerAngles; // get the current rotation part of the transform component of this game object. This problem can be solved w/o using this but this way is way more sleaker.
             Vector3 newRotation = new Vector3(currentRotation.x, currentRotation.y + 180f, currentRotation.z); // calculate the new rotation by adding 180 degrees to the y rotation and keeping the other axis the same
-            transform.rotation = Quaternion.Euler(newRotation); // assign the new rotation to the rotation part of the transform component of this game object
+
+            //transform.localRotation = Quaternion.Euler(newRotation); // assign the new rotation to the rotation part of the transform component of this game object
+            playerRB.MoveRotation(Quaternion.Euler(newRotation).x);
         }
+    }
+    */
+    private void AutoJump()
+    {
+        Vector2 force = new Vector2(0f, 15f);
+        playerRB.AddForce(force, ForceMode2D.Impulse);
+        Invoke("AutoJump", 3f);
     }
 
 }
